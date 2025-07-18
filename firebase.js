94f789edbbc49aaca9e1595befc3cb4541d82de0
KKOUADIO1 <86641382+KKOUADIO1@users.noreply.github.com>
2025-07-17 20:56:49 -0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import this in script.js using `import './firebase.js';`
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
+// TODO: Replace with your own Firebase config
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-app.firebaseapp.com",
+  databaseURL: "https://your-app.firebaseio.com",
+  projectId: "your-app",
+  storageBucket: "your-app.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db, ref, push };
